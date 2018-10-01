e5cbf41e9b4678a6c28e5efbd4fe568a8e2a2bb1
Mumtaz.Khan <mumtaz.khan@daugherty.com>
2018-09-30 21:53:01 -0500

    using method ref instead

--- src/main/java/com/design/strategy/AssetCalculator.java
+++ src/main/java/com/design/strategy/AssetCalculator.java
@@ -8,7 +8,8 @@ public class AssetCalculator {
     public static Long calculateAssetValue(List<Asset> assets, Predicate<Asset> selector) {
         Long sum = assets.stream()
                 .filter(selector)
-                .mapToLong(asset -> asset.getValue())
+                //.mapToLong(asset -> asset.getValue())
+                .mapToLong(Asset::getValue) // using method ref instead
                 .sum();
 
         return sum;
