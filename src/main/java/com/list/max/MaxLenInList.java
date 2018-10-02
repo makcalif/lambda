a15884a204b297e400456a899c5b330914c7f75b
Mumtaz.Khan <mumtaz.khan@daugherty.com>
2018-10-02 00:11:01 -0500

    longest line in a file (2 approaches)

--- src/main/java/com/list/max/MaxLenInList.java
+++ src/main/java/com/list/max/MaxLenInList.java
@@ -1,10 +1,11 @@
 package com.list.max;
 
 import java.io.*;
+import java.util.Comparator;
 
 public class MaxLenInList {
 
-    public static int getLongestLine(File file) throws IOException {
+    public static int getLongestLineLength(File file) throws IOException {
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -16,5 +17,23 @@ public class MaxLenInList {
                 .max()
                 .getAsInt();
         return i;
+    }
+
+    public static String getLongestLineApproach1(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String longest = bufferedReader.lines()
+                .reduce((x, y) -> x.length() > y.length() ? x : y)
+                .get();
+        return longest;
+    }
+
+    public static String getLongestLineApproach2(File file) throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String longest = bufferedReader.lines()
+                .max(Comparator.comparingInt(String::length))
+                .get();
+        return longest;
     }
 }
--- src/test/java/com/list/max/MaxLenInListTest.java
+++ src/test/java/com/list/max/MaxLenInListTest.java
@@ -1,6 +1,7 @@
 package com.list.max;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -8,10 +9,28 @@ import java.net.URISyntaxException;
 
 public class MaxLenInListTest {
 
+    File file;
+
+    @Before
+    public void readFile() {
+        file = new File("src/test/resource/fileWithLines.txt");
+    }
+
     @Test
-    public void testMaxInFile() throws URISyntaxException, FileNotFoundException, IOException {
-        File file = new File("src/test/resource/fileWithLines.txt");
-        int len = MaxLenInList.getLongestLine(file);
+    public void testMaxLineLength() throws  IOException {
+        int len = MaxLenInList.getLongestLineLength(file);
         Assert.assertTrue("len should not be less than 100", len > 100);
+    }
+
+    @Test
+    public void testMaxLineApproach1 () throws IOException{
+        String longest = MaxLenInList.getLongestLineApproach1(file);
+        Assert.assertTrue("longest line should be more than 100 chars", longest.length() > 100);
+    }
+
+    @Test
+    public void testMaxLineApproach2 () throws IOException{
+        String longest = MaxLenInList.getLongestLineApproach1(file);
+        Assert.assertTrue("longest line should be more than 100 chars", longest.length() > 100);
     }
 }
