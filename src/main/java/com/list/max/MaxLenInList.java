d159a72e7974537fcf7162c4e9a5bf5942c01f46
x239954 <Mumtaz.Khan@wnco.com>
2018-10-03 16:23:27 -0500

    added try-with-resources for buffered reader

--- src/main/java/com/list/max/MaxLenInList.java
+++ src/main/java/com/list/max/MaxLenInList.java
@@ -7,33 +7,39 @@ public class MaxLenInList {
 
     public static int getLongestLineLength(File file) throws IOException {
         FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        int i;
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
 
 //        bufferedReader.lines()
 //                .forEach(l -> System.out.printf("%d : %s\n", l.length(), l));
 
-        int i= bufferedReader.lines()
-                .mapToInt(String::length)
-                .max()
-                .getAsInt();
+            i = bufferedReader.lines()
+                    .mapToInt(String::length)
+                    .max()
+                    .getAsInt();
+        }
         return i;
     }
 
     public static String getLongestLineApproach1(File file) throws IOException {
         FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String longest = bufferedReader.lines()
-                .reduce((x, y) -> x.length() > y.length() ? x : y)
-                .get();
-        return longest;
+        String longest;
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            longest = bufferedReader.lines()
+                    .reduce((x, y) -> x.length() > y.length() ? x : y)
+                    .get();
+            return longest;
+        }
     }
 
     public static String getLongestLineApproach2(File file) throws IOException {
         FileReader fileReader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String longest = bufferedReader.lines()
-                .max(Comparator.comparingInt(String::length))
-                .get();
-        return longest;
+        String longest;
+        try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            longest = bufferedReader.lines()
+                    .max(Comparator.comparingInt(String::length))
+                    .get();
+            return longest;
+        }
     }
 }
