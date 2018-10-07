2d851bd0cd0da4f18b8dd582c046a84ef0b0881a
Mumtaz.Khan <mumtaz.khan@daugherty.com>
2018-10-07 16:51:24 -0500

    map list of Students by subjects

--- src/main/java/com/list/transform/ListToMap.java
+++ src/main/java/com/list/transform/ListToMap.java
@@ -1,6 +1,7 @@
 package com.list.transform;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BinaryOperator;
@@ -10,6 +11,37 @@ import java.util.stream.Collectors;
 public class ListToMap {
     public static void main(String[] args) {
 
+        mapUsingPropertyAsKeyAndObjectAsValue();
+
+        // find student with best grade
+        getMaxAndMinByGrade();
+
+        // create a map where subject is key and value is list of Students
+        mapListOfStudentsBySubjects();
+
+    }
+
+    private static void mapListOfStudentsBySubjects() {
+        List<Student> students = getStudents();
+        Map<String, List<Student>> map = students.stream()
+                .collect(Collectors.groupingBy(Student::getSubject));
+
+        map.entrySet().stream()
+                .forEach(v -> System.out.println(v));
+    }
+
+    private static void getMaxAndMinByGrade() {
+        List<Student> students = getStudents();
+        students.stream()
+                .min(Comparator.comparing(Student::getGrade).reversed())
+                .ifPresent(max -> System.out.println(String.format("Student with highest greade: %s", max)));
+
+        students.stream()
+                .min(Comparator.comparing(Student::getGrade))
+                .ifPresent(min -> System.out.println(String.format("Student with lowest greade: %s", min)));
+    }
+
+    private static void mapUsingPropertyAsKeyAndObjectAsValue() {
         List<Student> students = getStudents();
 
         // merge func to take care of duplicate keys in Student -> subject
@@ -28,11 +60,11 @@ public class ListToMap {
 
     private static List<Student> getStudents () {
         List<Student> students = Arrays.asList(
-          new Student("John", "Chemistry"),
-          new Student("Derek", "Physics"),
-          new Student("Kirk", "Biology"),
-          new Student("Norman", "Chemistry"),
-          new Student("Norman", "Biology")
+          new Student("John", "Chemistry", 55),
+          new Student("Derek", "Physics", 65),
+          new Student("Kirk", "Biology", 75),
+          new Student("Norman", "Chemistry", 85),
+          new Student("Norman", "Biology", 95)
         );
 
         return students;
@@ -41,10 +73,12 @@ public class ListToMap {
     private static class Student {
         private String name;
         private String subject;
+        private Integer grade;
 
-        public Student(String name, String subject) {
+        public Student(String name, String subject, Integer grade) {
             this.name = name;
             this.subject = subject;
+            this.grade = grade;
         }
 
         public String getName() {
@@ -63,11 +97,20 @@ public class ListToMap {
             this.subject = subject;
         }
 
+        public Integer getGrade() {
+            return grade;
+        }
+
+        public void setGrade(Integer grade) {
+            this.grade = grade;
+        }
+
         @Override
         public String toString() {
             return "Student{" +
                     "name='" + name + '\'' +
                     ", subject='" + subject + '\'' +
+                    ", grade=" + grade +
                     '}';
         }
     }
